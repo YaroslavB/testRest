c8b1432a7b00e304dd83a5ca3dae72bc7aefd5be
Yaroslav <yaroslav.bailo@gmail.com>
2020-12-10 01:34:13 +0200

    - [fix] - travis2

--- app/src/Controller/LunController.php
+++ app/src/Controller/LunController.php
@@ -1,9 +1,6 @@
 <?php
 
-
     namespace App\Controller;
-
-
 
     use App\Service\Scraper;
     use Psr\Http\Message\ServerRequestInterface;
@@ -13,26 +10,25 @@
      * Class LunController
      * @package App\Controller
      */
-    class LunController
+class LunController
+{
+    /**
+     * @param ServerRequestInterface $request
+     * @param Scraper $scraper
+     * @return HtmlResponse
+     */
+    public function __invoke(ServerRequestInterface $request): HtmlResponse
     {
-        /**
-         * @param ServerRequestInterface $request
-         * @param Scraper $scraper
-         * @return HtmlResponse
-         */
-        public function __invoke(ServerRequestInterface $request): HtmlResponse
-        {
-            $data = [   'core' => 123,
-                        'more' => 456,];
+        $data = [   'core' => 123,
+                    'more' => 456,];
 
-            $scraper = new Scraper('http://markusos.github.io/');
-            $scraper->loadData('/');
-            $siteTitle = $scraper->getNode('//a[@class="site-title"]');
-            echo $siteTitle->nodeValue;
+        $scraper = new Scraper('http://markusos.github.io/');
+        $scraper->loadData('/');
+        $siteTitle = $scraper->getNode('//a[@class="site-title"]');
+        echo $siteTitle->nodeValue;
 
 
 
-           // return  new HtmlResponse( '<p>egsdfsdf</p>');
-        }
-
+       // return  new HtmlResponse( '<p>egsdfsdf</p>');
     }
+}
--- app/src/Service/Scraper.php
+++ app/src/Service/Scraper.php
@@ -6,70 +6,65 @@
     use GuzzleHttp\Exception\ConnectException;
     use http\Exception\RuntimeException;
 
-    class Scraper
+class Scraper
+{
+
+    private $webClient;
+
+    private $dom;
+
+    /**
+     * Scraper constructor.
+     * @param $site
+     * @param int $timeout
+     */
+    public function __construct($site, $timeout = 2)
     {
-
-        private  $webClient;
-
-        private  $dom;
-
-        /**
-         * Scraper constructor.
-         * @param $site
-         * @param int $timeout
-         */
-        public function __construct($site, $timeout = 2)
-        {
-            $this->webClient = new Client([
-                'base_uri' => $site,
-                'timeout' => $timeout
-            ]);
-        }
-
-        public function loadData($page)
-        {
-            try {
-                $response = $this->webClient->get($page);
-            }catch (ConnectException $e)
-            {
-                throw  new  RuntimeException($e->getHandlerContext()['error']);
-            }
-            $html = $response->getBody();
-            $this->dom = new \DOMDocument;
-            libxml_use_internal_errors(true);
-            $this->dom->loadHTML($html);
-            libxml_clear_errors();
-            return $this;
-
-        }
-
-        /**
-         * @param $xpath
-         * @param null $parent
-         * @return \DOMNodeList
-         */
-        private function getNodes($xpath, $parent = null): \DOMNodeList
-        {
-            $DomXpath = new \DOMXPath($this->dom);
-            $nodes = $DomXpath->query($xpath,$parent);
-            return $nodes;
-        }
-
-        /**
-         * @param $xpath
-         * @param null $parent
-         * @return mixed
-         */
-        public  function  getNode($xpath, $parent =null)
-        {
-            $nodes = $this->getNodes($xpath, $parent);
-            if ($nodes->length === 0) {
-                throw new \RuntimeException("No matching node found");
-            }
-
-            return $nodes[0];
-
-        }
-
-
+        $this->webClient = new Client([
+            'base_uri' => $site,
+            'timeout' => $timeout
+        ]);
     }
+
+    public function loadData($page)
+    {
+        try {
+            $response = $this->webClient->get($page);
+        } catch (ConnectException $e) {
+            throw  new  RuntimeException($e->getHandlerContext()['error']);
+        }
+        $html = $response->getBody();
+        $this->dom = new \DOMDocument();
+        libxml_use_internal_errors(true);
+        $this->dom->loadHTML($html);
+        libxml_clear_errors();
+        return $this;
+    }
+
+    /**
+     * @param $xpath
+     * @param null $parent
+     * @return \DOMNodeList
+     */
+    private function getNodes($xpath, $parent = null): \DOMNodeList
+    {
+        $DomXpath = new \DOMXPath($this->dom);
+        $nodes = $DomXpath->query($xpath, $parent);
+        return $nodes;
+    }
+
+    /**
+     * @param $xpath
+     * @param null $parent
+     * @return mixed
+     */
+    public function getNode($xpath, $parent = null)
+    {
+        $nodes = $this->getNodes($xpath, $parent);
+        if ($nodes->length === 0) {
+            throw new \RuntimeException("No matching node found");
+        }
+
+        return $nodes[0];
+    }
+}
